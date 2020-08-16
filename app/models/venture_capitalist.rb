e3bd982d499df1a4afed8a9343f01717373073e5
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-08-16 11:54:12 -0400

    all but 2 methods

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,49 @@
-class VentureCapitalist
+class VentureCapitalist 
+    attr_accessor :name, :total_worth
+    @@all = []
 
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        self.all.filter{|vc| vc.total_worth > 1000000000}
+    end    
+
+    def offer_contract(startup, type, investment)
+        FundingRound.new(startup, self, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select do |funding| 
+            funding.venture_capitalist == self
+        end
+    end
+
+    def portfolio
+        total = funding_rounds.map{|fr|fr.startup}
+            total.uniq 
+    end
+
+    def biggest_investment
+        self.funding_rounds.reduce do |fr1, fr2|
+            if fr1.investment > fr2.investment
+                fr1
+            else
+                fr2
+            end
+        end
+    end
+
+    # def invested(domain)
+    #     self.funding_rounds.map{|s| s.domain == domain}
+      
+    # end
+        
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,23 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+uber = Startup.new("uber", "john smith", "google")
+dominos = Startup.new("dominos", "Tony Pizza", "microsoft")
+blimpies = Startup.new("blimpies", "Tommy Salami", "minecraft")
+tacobell = Startup.new("tacobell", "john smith", "firefox")
+
+billgates= VentureCapitalist.new("Bill Gates", 56000000000)
+elonmusk = VentureCapitalist.new("Elon Musk", 32000000000)
+tommy = VentureCapitalist.new("Tommy", 32000)
+
+f1 = FundingRound.new(uber, elonmusk, "seed", 45000000.00)
+f2 = FundingRound.new(dominos, billgates, "pres eed", 600000000.00)
+f3 = FundingRound.new(blimpies, tommy, "series a", 20000.00)
+f4 = FundingRound.new(uber, billgates, "seed b", 500000)
+f5= FundingRound.new(uber, elonmusk, "seed a", 1505000)
+
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
